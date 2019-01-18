2a35e2b47702b5f694ec65ce4bec0d0ea719af51
Caleb Kinney <cakinney@gmail.com>
2019-01-18 05:36:36 -0500

    Remove unused variables
    - Remove defaultValue
    - Remove  seconds, values import

--- src/policies.ts
+++ src/policies.ts
@@ -1,5 +1,3 @@
-export const defaultValue = "default";
-
 export enum PolicyOptions {
   NotSet,
   DefaultHeader,
--- src/utils.ts
+++ src/utils.ts
@@ -7,9 +7,7 @@ import {
   XXP,
   Feature,
   Referrer,
-  Server,
-  seconds,
-  values
+  Server
 } from "./policies";
 
 export interface SecureCookieOptions {
